48068bf10ffc5b53e6bb35d2c5814ff4d9741a2c
Szymon <gutek.gutomski@gmail.com>
2024-12-02 23:07:52 +0100

    Dodano klase

--- GraPlanszowa/Program.cs
+++ GraPlanszowa/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
 
-Console.WriteLine("Hello, World!");
+namespace BoardGameSimulator
+{
+    // Klasa reprezentująca gracza
+    class Player
+    {
+        public string Name { get; set; }
+        public int Position { get; set; }
+        public int Points { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Position = 0; // Startowa pozycja
+            Points = 0; // Początkowa liczba punktów
+        }
+
+        public void Move(int steps)
+        {
+            Position += steps;
+            Console.WriteLine($"{Name} przesuwa się o {steps} pól na pozycję {Position}.");
+        }
+    }
+}
+
+    
